b07c766940d41267cf6d630faf7e34dccd9eeaf5
Owen Wang <owenwang0099@gmail.com>
2022-12-22 15:15:09 -0500

    TUI

---
+++ src/main/java/megabudgetstonks/MegaBudgetStonks.java
@@ -0,0 +1,11 @@
+package megabudgetstonks;
+
+import com.googlecode.lanterna.SGR;
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+import java.io.IOException;
+
+public class MegaBudgetStonks {
+}
