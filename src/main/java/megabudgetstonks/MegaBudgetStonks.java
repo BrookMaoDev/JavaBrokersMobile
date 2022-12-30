a35b58ab32cc0c7a45a4cb4c3ee5fedf8221d505
Owen Wang <owenwang0099@gmail.com>
2022-12-30 00:46:22 -0500

    Basic stock TUI

--- src/main/java/megabudgetstonks/MegaBudgetStonks.java
+++ src/main/java/megabudgetstonks/MegaBudgetStonks.java
@@ -1,11 +1,51 @@
 package megabudgetstonks;
 
-import com.googlecode.lanterna.SGR;
-import com.googlecode.lanterna.TerminalPosition;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.gui2.*;
+import com.googlecode.lanterna.gui2.table.Table;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
-import java.io.IOException;
 
 public class MegaBudgetStonks {
+    private API api = new API();
+    private StockData stockData = new StockData();
+
+    public void megabudgetstonks() {
+        try {
+            // Setup terminal/console and screen layers
+            Terminal terminal = new DefaultTerminalFactory().createTerminal();
+            Screen screen = new TerminalScreen(terminal);
+            screen.startScreen();
+
+            // Create panels for each ticker
+            Panel mainPanel = new Panel();
+            Table<String> table = new Table<String>("TICKER", "PRICE", "CHANGE", "% CHANGE");
+
+            ArrayList<String> data = stockData.getData("trending-tickers");
+            for (int i = 0; i < 10; i++) {
+                api.setSymbol(data.get(i));
+                table.getTableModel().addRow(api.getSymbol(), api.getPrice().toString(), api.getChange().toString(),
+                        api.getPercentChange().toString());
+            }
+            mainPanel.addComponent(table);
+
+            // Create window
+            BasicWindow window = new BasicWindow();
+            window.setHints(Arrays.asList(Window.Hint.CENTERED, Window.Hint.FULL_SCREEN, Window.Hint.NO_DECORATIONS));
+            window.setComponent(mainPanel);
+
+            // Create gui and start gui
+            MultiWindowTextGUI gui = new MultiWindowTextGUI(screen, new DefaultWindowManager(),
+                    new EmptySpace(TextColor.ANSI.BLUE));
+            gui.addWindowAndWait(window);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
 }
