11bbe7caf275c6473cac1ad56479b215972f464a
Wing Li <wing.li@student.tdsb.on.ca>
2023-01-11 12:56:08 -0500

    ownedstock mutators

--- src/main/java/JBKRMobile/OwnedStock.java
+++ src/main/java/JBKRMobile/OwnedStock.java
@@ -9,11 +9,29 @@ public class OwnedStock {
         this.quantity = quantity;
     }
 
+    // Accessors
     public String getTicker() {
         return ticker;
     }
 
     public long getQuantity() {
         return quantity;
+    }
+
+    public void addQuantity(int amount) {
+        quantity += amount;
+    }  
+
+    /**
+     * Subtracts an int from quantity. If amount is greater than quantity, then returns false before subtracting. 
+     * Otherwise, returns true
+     * @param amount int
+     */
+    public boolean subtractQuantity (int amount) {
+        if (quantity - amount < 0) {
+            return false;
+        }
+        quantity -= quantity;
+        return true;
     }
 }
