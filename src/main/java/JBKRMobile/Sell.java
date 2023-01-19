2a62e17b128e2e062552451cf1acbdda37c54894
WingLi2 <91137325+WingLi2@users.noreply.github.com>
2023-01-18 20:10:16 -0500

    removed costoftransaction

--- src/main/java/JBKRMobile/Sell.java
+++ src/main/java/JBKRMobile/Sell.java
@@ -4,12 +4,6 @@ public class Sell extends Transaction {
     public Sell(String date, String ticker, int quantity, double price) {
         super(date, ticker, quantity, price);
     }
-
-    // The profit gained from this transaction
-    // Profit is calculated by quantity * price;
-    public double costOfTransaction() {
-        return quantity * price;
-    }
     
     public String fileString() {
         return "sell\n"+ date + "\n" + ticker + "\n" + quantity + "\n" + price;
