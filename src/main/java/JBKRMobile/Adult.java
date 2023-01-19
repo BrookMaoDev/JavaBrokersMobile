728d3997e99abee4c41be6f3d45eb39320e9ea6c
Wing Li <wing.li@student.tdsb.on.ca>
2023-01-19 12:52:59 -0500

    sortportfolio

--- src/main/java/JBKRMobile/Adult.java
+++ src/main/java/JBKRMobile/Adult.java
@@ -35,12 +35,12 @@ public class Adult extends Investor {
             OwnedStock boughtStock = new OwnedStock(ticker, quantity);
             portfolio.add(boughtStock);
             stocksInPortfolio++;
+            sortPortfolio();
         } else {
             // The user owns this stock
             portfolio.get(tickerIndex).addQuantity(quantity);
         }
 
-        sortPortfolio();
         return 1;
     }
 
