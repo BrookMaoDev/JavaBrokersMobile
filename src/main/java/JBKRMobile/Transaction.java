6a3230cb67168b1818e93e3ac721d2e93ff862e2
WingLi2 <91137325+WingLi2@users.noreply.github.com>
2023-01-21 15:53:35 -0500

    transaction documentation

--- src/main/java/JBKRMobile/Transaction.java
+++ src/main/java/JBKRMobile/Transaction.java
@@ -15,6 +15,15 @@ public class Transaction {
     protected int quantity;
     protected double price;
 
+    /**
+     * @param transactionType: the type of the transaction (currently either "buy" or "sell")
+     * @param date:            the date the transaction took place in the form "YYYY-MM-DD"
+     * @param ticker:          the ticker of the stock exchanged in the transaction
+     * @param quantity:        the quantity of the stock exchanged in the transaction
+     * @param price:           the unit price of the stock exchanged in the transaction
+     * @return does not return anything.
+     *         Creates a new Transaction object
+     */
     public Transaction(String transactionType, String date, String ticker, int quantity, double price) {
         this.transactionType = transactionType;
         this.date = date;
@@ -23,25 +32,46 @@ public class Transaction {
         this.price = price;
     }
 
+    // Accessors
     public String getDate() {
         return date;
     }
 
+    /**
+     * Determines whether or not this transaction occured in the provided date
+     *
+     * @param date: The date to be compared in the form "YYYY-MM-DD"
+     * @return boolean
+     *         true if the provided date matches the date this transaction was made
+     *         false otherwise
+     */
     public boolean dateEquals(String date) {
         return this.date.equals(date);
     }
 
-    // The profit gained from this transaction
-    // Profit is calculated by quantity * price;
+    /**
+     * The cost of the transaction
+     * Cost is calculated by quantity * price
+     * @return double
+     */
     public double costOfTransaction() {
         return quantity * price;
     }
 
-    // String that contains information about the transaction to write to a file
+    /**
+     * Returns the information about this object in a format
+     * to be written to a file
+     * @return String
+     */
     public String fileString() {
         return String.format("%s\n%s\n%s\n%d\n%.2f", transactionType, date, ticker, quantity, price);
     }
-
+    
+    /**
+     * Returns the information about this object in a format
+     * to be displayed to the user
+     * @return String
+     */
     public String toString() {
         return String.format("%s\nDate: %s\nTicker: %s\nQuantity: %d\nPrice: %s\n", transactionType, date, ticker,
                 quantity, NumberFormat.getCurrencyInstance().format(price));
